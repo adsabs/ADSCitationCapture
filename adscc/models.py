a4258df58da6b458bf9e97ac7099749baa66ca00
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2017-09-08 20:33:39 +0200

    Refactoring to group delta computation operations

--- adscc/models.py
+++ adscc/models.py
@@ -1,26 +1,34 @@
-from sqlalchemy import Column, DateTime, String, Integer, func
-from sqlalchemy.dialects.postgresql import JSON, JSONB
+from sqlalchemy import Column, DateTime, String, Text, Integer, func
+from sqlalchemy.dialects.postgresql import ENUM, JSON, JSONB
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-#class RawCitation(Base):
-    #__tablename__ = 'raw_citation'
-    #__table_args__ = ({"schema": "testing_schema"})
-    ##id = Column(Integer, primary_key=True)
-    #bibcode = Column(String(19), primary_key=True)
-    #payload = Column(JSON)
+class RawCitation(Base):
+    __tablename__ = 'raw_citation'
+    __table_args__ = ({"schema": "public"})
+    id = Column(Integer, primary_key=True)
+    bibcode = Column(String(19))
+    payload = Column(JSONB) # Binary, faster than JSON (requires postgres >9.4)
 
-
-def build_raw_citation_table(schema="public"):
-    class RawCitation(Base):
-        __tablename__ = 'raw_citation'
-        __table_args__ = ({"schema": schema})
-        id = Column(Integer, primary_key=True)
-        bibcode = Column(String(19))
-        payload = Column(JSON)
-        #payload = Column(JSONB) # Binary, faster searches (requires postgres 9.4)
-    return RawCitation
-
-RawCitation = build_raw_citation_table()
+class CitationChanges(Base):
+    __tablename__ = 'citation_changes'
+    __table_args__ = ({"schema": "public"})
+    id = Column(Integer, primary_key=True)
+    new_id = Column(Integer)
+    new_citing = Column(Text())
+    new_cited = Column(Text())
+    new_doi = Column(Text())
+    new_pid = Column(Text())
+    new_url = Column(Text())
+    new_data = Column(Text())
+    new_score = Column(Text())
+    previous_citing = Column(Text())
+    previous_cited = Column(Text())
+    previous_doi = Column(Text())
+    previous_pid = Column(Text())
+    previous_url = Column(Text())
+    previous_data = Column(Text())
+    previous_score = Column(Text())
+    status = Column(ENUM('NEW', 'DELETED', 'UPDATED', name='status_type'))
 
