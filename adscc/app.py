d4cd077495b2864810df077ebb5200972569fff2
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2017-10-20 21:43:30 +0200

    Better handling of HTTP error from doi.org

--- adscc/app.py
+++ adscc/app.py
@@ -4,6 +4,7 @@ from models import *
 class ADSCitationCaptureCelery(ADSCelery):
     def attempt_recovery(self, task, args=None, kwargs=None, einfo=None, retval=None):
         """
-        If task fails, keep trying forever
+        If task fails after 3 attempts...
         """
-        task.apply_async(args=args, kwargs=kwargs)
+        #task.apply_async(args=args, kwargs=kwargs)
+        pass
--- adscc/doi.py
+++ adscc/doi.py
@@ -6,6 +6,9 @@ from adsputils import setup_logging
 logger = setup_logging(__name__)
 
 def is_software(base_doi_url, doi):
+    is_software = False
+    record_found = False
+    try_later = False
     doi_endpoint = base_doi_url + doi
     headers = {}
     ## Supported content types: https://citation.crosscite.org/docs.html#sec-4
@@ -18,25 +21,33 @@ def is_software(base_doi_url, doi):
         r = requests.get(doi_endpoint, data=json.dumps(data), headers=headers, timeout=timeout)
     except:
         logger.exception("HTTP request to DOI service failed: %s", doi_endpoint)
-        success = False
+        try_later = True
     else:
         if r.status_code == 406:
             logger.error("No answer from doi.org with the requested format (%s) for: %s", headers["Accept"], doi_endpoint)
-            raise Exception("No answer from doi.org with the requested format ({}) for: {}".format(headers["Accept"], doi_endpoint))
-        success = True
+            #raise Exception("No answer from doi.org with the requested format ({}) for: {}".format(headers["Accept"], doi_endpoint))
+        elif r.status_code == 404:
+            logger.error("Entry not found (404 HTTP error code): %s", doi_endpoint)
+            #raise Exception("Entry not found (404 HTTP error code): {}".format(doi_endpoint))
+        elif not r.ok:
+            # Rest of bad status codes
+            try_later = True
+        else:
+            record_found = True
 
-    if not success or not r.ok:
+    if try_later:
+        # Exceptions make the task to fail, and the framework will re-try automatically later on
         logger.error("HTTP request to DOI service failed: %s", doi_endpoint)
         raise Exception("HTTP request to DOI service failed: {}".format(doi_endpoint))
 
-    is_software = False
-    try:
-        root = etree.fromstring(r.content)
-    except:
-        pass
-    else:
-        resource_type = root.find("{http://datacite.org/schema/kernel-3}resourceType")
-        if resource_type is not None:
-            resource_type_general = resource_type.get('resourceTypeGeneral')
-            is_software = resource_type_general is not None and resource_type_general.lower() == "software"
+    if record_found:
+        try:
+            root = etree.fromstring(r.content)
+        except:
+            pass
+        else:
+            resource_type = root.find("{http://datacite.org/schema/kernel-3}resourceType")
+            if resource_type is not None:
+                resource_type_general = resource_type.get('resourceTypeGeneral')
+                is_software = resource_type_general is not None and resource_type_general.lower() == "software"
     return is_software
